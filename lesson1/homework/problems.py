5e0e834f9628e8c59f015ef798884f7a7e287d4a
Arnav Desetty <desettyarnav@gmail.com>
2025-11-11 18:44:00 -0800

    Finshed leson 1 homworek.

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,32 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
-
+luckynumber = 2
+print(luckynumber)
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
-
+sleep = 10
+print(sleep)
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
-
+fruit = "strawberry"
+print("I like", fruit)
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
+city='Redmond'
+contry='America'
+print(city)
+print(contry)
+print(city, "\n", contry)
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+petname='tom'
+petage=9
+pettype='Dog'
+print("my pet(", pettype, ") name is", petname, "and its age is", petage)
+
